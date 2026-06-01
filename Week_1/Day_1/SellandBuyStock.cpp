ccd727d98439cf16fea8099a30c068004791f30c
bitsofprakhar <dhaundiyalprakhar2006@gmail.com>
2026-06-01 17:23:50 +0530

    Week 1 Day 1 solutions

---
+++ Week_1/Day_1/SellandBuyStock.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minprice=prices[0];
+        int maxprofit=0;
+        for (int i =1; i<prices.size(); i++){
+            int cost= prices[i]-minprice;
+             maxprofit=max(maxprofit,cost);
+             minprice=min(minprice,prices[i]);
+        }
+        return maxprofit;
+    }
+};
