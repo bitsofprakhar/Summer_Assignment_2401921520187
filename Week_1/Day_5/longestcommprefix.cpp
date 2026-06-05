a4e79ace8165978d883df1404c5e9c7109abd7b7
bitsofprakhar <dhaundiyalprakhar2006@gmail.com>
2026-06-05 15:22:01 +0530

    Week 1 Day 5

---
+++ Week_1/Day_5/longestcommprefix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(),strs.end());
+        string ans = "";
+        string first= strs[0];
+        string last= strs[strs.size()-1];
+
+        for(int i =0; i<min(first.size(),last.size()); i++){
+            if(first[i]!=last[i]) {break;}
+            ans+=first[i];
+        }
+        return ans;
+    }
+};
